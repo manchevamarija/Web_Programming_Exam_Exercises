6bb9e8f5730f155c69e1ebeced5e2ec4f54aff52
Marija <manchevaa06@gmail.com>
2026-01-21 16:48:54 +0100

    AddJuneG1Task

---
+++ JuniG1-master/JuniG1-master/june2025-g1/src/main/java/mk/ukim/finki/wp/june2025g1/repository/FounderRepository.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.wp.june2025g1.repository;
+
+import mk.ukim.finki.wp.june2025g1.model.Founder;
+
+public interface FounderRepository extends JpaSpecificationRepository<Founder,Long> {
+}
